650c9d6b78c01effa27d0328dde2a1dc50e1682d
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2025-02-15 23:36:14 +0900

    fix: Order.js 수정
    - param 지정자 삭제
    - 기본값 삭제

--- models/Order.js
+++ models/Order.js
@@ -1,21 +1,12 @@
 class Order {
-    /**
-     * @param {number} orderId - 거래 ID (Primary Key)
-     * @param {number} requesterId - 신청자 ID (Foreign Key)
-     * @param {number} accepterId - 구매자 ID (Foreign Key)
-     * @param {number | null} orderState - 거래 상태 (nullable)
-     * @param {Date | null} createdAt - 거래 체결 일시 (nullable)
-     * @param {Item[] | null} items - 품목 리스트 (nullable) Item도 만들어야 함
-     * @param {number | null} fee - 수고비 (nullable)
-     */
-    constructor(orderId, requesterId, accepterId, orderState = null, createdAt = null, items = null, fee = null) {
-        this.orderId = orderId;
-        this.requesterId = requesterId;
-        this.accepterId = accepterId;
-        this.orderState = orderState;
-        this.createdAt = createdAt;
-        this.items = items;
-        this.fee = fee;
+    constructor(orderId, requesterId, accepterId, orderState, createdAt, items, fee) {
+        this.orderId = orderId;         // PK: (Int) 거래 ID
+        this.requesterId = requesterId; // FK: (Int) 신청자 ID
+        this.accepterId = accepterId;   // FK: (Int) 구매대행자 ID
+        this.orderState = orderState;   // (Int) 거래 상태
+        this.createdAt = createdAt;     // (Date) 거래 체결일시
+        this.items = items;             // (Item[]) 품목 리스트
+        this.fee = fee;                 // (Int) 수고비
     }
 }
 
